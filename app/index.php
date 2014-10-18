39e247312a5b7deffbc807ede6f95f34dff43b33
Oscar Javier Mosh <opatino@experienciasxcaret.com>
2014-10-18 12:39:43 -0500

    StyleShell Init Files
    Inspired by FastShell and FireShell

---
+++ app/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Malecón Américas</title>
+</head>
+<body>
+	<h1>Test</h1>
+	<section>
+		<h2>Login</h2>
+		<aside>Indicators</aside>
+		<article>
+			Bienvenido a Malecón
+			<img src="http://placekitten.com/600/400" alt="">
+			<img src="http://placekitten.com/600/400" alt="">
+			<img src="http://placekitten.com/600/400" alt="">
+			<img src="http://placekitten.com/600/400" alt="">
+		</article>
+	</section>
+	<section>
+		<h2>Counter</h2>
+	</section>
+	<section>
+		<h2>Welcoming</h2>
+	</section>
+	
+</body>
+</html>
